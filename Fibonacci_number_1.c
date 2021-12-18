7c685d351eb9507082b44646a8d6320966e2a6c5
Blue-Pocke <worldasakindoflarge@gmail.com>
2021-12-18 09:16:14 +0900

    Fibonacci_number_1.c

---
+++ Fibonacci_number_1.c
@@ -0,0 +1,26 @@
+/* 2021.12.17 Fri. 21:27 - 21:52 */
+/* Fibonacci number */
+
+#include<stdio.h>
+
+int main(){
+
+	int my_array[20];
+	int i;
+	my_array[0]=0;
+	my_array[1]=1;
+
+		for (i=2; i<20; i++){
+			my_array[i] = my_array[i-2] + my_array[i-1];
+			}
+
+		for (i=0; i<20; i++){
+			printf("%d\t",my_array[i]);
+			}
+	return 0;
+}
+
+
+
+
+
